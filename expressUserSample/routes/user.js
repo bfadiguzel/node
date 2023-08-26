85f0d553db93e4ce1a19ba1e35ae01e39b42be15
Bilal Fatih Adıgüzel <bilalgsda@gmail.com>
2023-08-27 01:02:33 +0300

    new example

---
+++ expressUserSample/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const {users} = require('./admin')
+
+router.get('/', (req, res, next) => {
+    console.log("You are in the Show Users page")
+    console.log(users)
+    res.render('show-user', { pageTitle: 'Show Users', path: '/' , users: users})
+})
+
+
+exports.router = router
